eec9745cb76aa7acf73fd513a61e467e9e9f03d6
takurateruyoshi <s2522068@stu.musashino-u.ac.jp>
2026-05-16 16:18:48 +0900

    Initial Commit

---
+++ PhotoMaster/PhotoMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMasterApp.swift
+//  PhotoMaster
+//
+//  Created by たくらてるよし on 2026/05/16.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
